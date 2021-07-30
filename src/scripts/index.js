c39786bfa0fee4fff097441546ba2ba2bb2ee077
Gutemberg Silva Filho <beguinhoboy@hotmail.com>
2021-07-30 18:52:55 -0300

    tittle and photo added

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,3 +3,13 @@ let sidebar = document.querySelector('.sidebar');
 function showSideBar(){
     sidebar.classList.toggle('active')
 }
+
+async function hideSideBar(){
+    const corpo =  await document.documentElement.querySelector('.container');
+    const width =  await corpo.clientWidth;
+    
+    if(width < 960 && sidebar.className === 'sidebar active'){
+        sidebar.classList.toggle('active')
+    }
+
+}
